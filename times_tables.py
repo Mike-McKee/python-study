8729c17a05a46a15c73aadfc7ec14d803190fce3
MIchael McKee <michaelmckee@MIchaels-MacBook-Pro.local>
2023-06-20 20:44:58 -0400

    finished code

--- times_tables.py
+++ times_tables.py
@@ -1,20 +1,6 @@
-# import pandas as pd
-
-# times_table = []
-# for i in range(1, 10):
-#     row = [i * j for j in range(1, 10)]
-#     times_table.append(row)
-
-# df = pd.DataFrame(
-#     times_table,
-#     index = [str(i) for i in range(1,10)],
-#     columns = [str(i) for i in range(1, 10)]
-#     )
-
-# print(df)
-
 import pandas as pd
 
+#this function creates the multiplication table 1-n, where n is the input
 def times_table(num_of_rows):
 
     data = []
@@ -31,17 +17,14 @@ def times_table(num_of_rows):
 
     return df
 
-query = int(input("Choose any integer greater than 0: "))
+while True:
+    #tells user to input a number for the times table
+    query = int(input("Choose any integer greater than 0: "))
 
-# have to fix this and add a while statement so that the sequence repeats until a valid number is entered
-# while status == True:
-#     status = True
+    if query > 0:
+        print(times_table(query))
+        break
 
-#     if query > 0:
-#         print(times_table(query))
-#         status = False
-    
-#     else:
-#         print(f"Try again. {query}")
-
-print(times_table(query))
+    #makes user try again and input a new number
+    else:
+        print(f"Try again. {query} is not greater than 0")
